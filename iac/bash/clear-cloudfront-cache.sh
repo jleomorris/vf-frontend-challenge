bf137abf7d37480a1ede4136a4b965a28b5c6cd2
Lee Richmond <lee@visfo.health>
2022-11-25 09:40:41 +0000

    chore: remove iac

--- iac/bash/clear-cloudfront-cache.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeuxo pipefail
-
-main() {
-	aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"
-}
-
-main "$@"
--- iac/terraform_aws/cicd-front-end/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  default_tags    = {
-    path_from_repo_root = var.path_from_repo_root
-  }
-}
